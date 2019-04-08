39b965b5c2a471b06396d14170d6ecb904f8c3eb
gurminderDeol <kaurdeol99@gmail.com>
2019-04-08 18:45:43 -0400

    first commit

---
+++ app/src/main/java/com/midtermmad3125/ui/Splash.java
@@ -0,0 +1,32 @@
+package com.midtermmad3125.ui;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.midtermmad3125.R;
+
+public class Splash extends AppCompatActivity {
+    static int SPLASH_TIME_OUT=4000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new  Handler().postDelayed(new Runnable()
+        {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(Splash.this,MainCityActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
+        {
+
+        }
+
+    }
+}
